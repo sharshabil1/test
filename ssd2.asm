d08b04dbb8a653acda0ea9dcb81fec6e2baede9d
sharshabil <122256306+sharshabil1@users.noreply.github.com>
2025-12-11 15:20:27 +0300

    Refactor segment finder tool in ssd2.asm

--- ssd2.asm
+++ ssd2.asm
@@ -1,114 +1,70 @@
-; =================================================================
-; Project: Scrolling Student ID "222438835"
-; Hardware: 
-;   - Port A (0FFE0H) -> Segments
-;   - Port B (0FFE2H) -> CAT / Select (Pin PB0)
-;   - VCC/GND -> Connected to Trainer Power
-; =================================================================
+; =============================================================
+; SEGMENT FINDER TOOL
+; -------------------------------------------------------------
+; This program lights up ONE segment at a time.
+; Watch your display and write down what you see.
+;
+; SEQUENCE:
+; 1. Lights PA0 only -> Wait -> Lights PA1 only -> Wait...
+; 2. Repeats forever.
+; =============================================================
 
 ORG 2000H
     JMP START
 
-; --- DATA TABLE ---
-; Digits: 2, 2, 2, 4, 3, 8, 8, 3, 5
-; Hex Codes: 
-; 2 = 5BH
-; 4 = 66H
-; 3 = 4FH
-; 8 = 7FH
-; 5 = 6DH
-; Space = 00H
-
-CODES:
-    DB 5BH      ; [0] '2'
-    DB 5BH      ; [1] '2'
-    DB 5BH      ; [2] '2'
-    DB 66H      ; [3] '4'
-    DB 4FH      ; [4] '3'
-    DB 7FH      ; [5] '8'
-    DB 7FH      ; [6] '8'
-    DB 4FH      ; [7] '3'
-    DB 6DH      ; [8] '5'
-    DB 00H      ; [9] Space (End)
-
 START:
-    ; 1. Initialize 8255 PPI (All Outputs)
+    ; Configure 8255
     MOV DX, 0FFE6H
     MOV AL, 80H
     OUT DX, AL
 
-MAIN_LOOP:
-    MOV SI, CODES   ; Point to start of ID
-    MOV CX, 9       ; Sequence Length (9 scrolling steps)
-
-SCROLL_SEQUENCE:
-    PUSH CX         ; Save counter
-
-    ; Load pair of numbers to display
-    MOV AL, BYTE [SI]    ; Left Digit
-    MOV BL, BYTE [SI+1]  ; Right Digit
-
-    ; --- SCROLL SPEED DELAY ---
-    ; Increased to 01FFH to make it slower and readable
-    MOV CX, 01FFH   
-
-MULTIPLEX_LOOP:
-    CALL DISPLAY_PAIR
-    LOOP MULTIPLEX_LOOP
-
-    POP CX          ; Restore counter
-    INC SI          ; Move to next number
-    LOOP SCROLL_SEQUENCE
-
-    JMP MAIN_LOOP   ; Repeat forever
-
-; --- SUBROUTINE: MULTIPLEXING ---
-DISPLAY_PAIR:
-    PUSH AX
-    PUSH DX
-
-    ; --------------------------------------
-    ; 1. DISPLAY RIGHT DIGIT (CAT = HIGH)
-    ; --------------------------------------
-    ; Select Right Digit (PB0 = 1)
-    MOV DX, 0FFE2H   ; Port B Address
-    MOV AH, 01H      ; Signal High
-    MOV AL, AH
+    ; Force Right Display ON (since we know this works)
+    MOV DX, 0FFE2H  ; Port B (CAT)
+    MOV AL, 01H     ; High Signal
     OUT DX, AL
 
-    ; Send Segment Data (Right Digit)
-    MOV DX, 0FFE0H   ; Port A Address
-    MOV AL, BL       ; Get Right Digit Data (BL)
-    OUT DX, AL       
-    
-    CALL DELAY_MUX   ; Wait for eye to see it
-
-    ; --------------------------------------
-    ; 2. DISPLAY LEFT DIGIT (CAT = LOW)
-    ; --------------------------------------
-    ; Select Left Digit (PB0 = 0)
-    MOV DX, 0FFE2H   ; Port B Address
-    MOV AL, 00H      ; Signal Low
+TEST_LOOP:
+    ; --- TEST PA0 ---
+    MOV DX, 0FFE0H
+    MOV AL, 01H     ; Binary 0000 0001
     OUT DX, AL
+    CALL LONG_DELAY
 
-    ; Send Segment Data (Left Digit)
-    MOV DX, 0FFE0H   ; Port A Address
-    POP AX           ; Restore original AX to get AL
-    PUSH AX          ; Put it back 
-    OUT DX, AL       ; Send Left Digit Data (AL)
-    
-    CALL DELAY_MUX   ; Wait for eye to see it
+    ; --- TEST PA1 ---
+    MOV AL, 02H     ; Binary 0000 0010
+    OUT DX, AL
+    CALL LONG_DELAY
 
-    POP DX
-    POP AX
-    RET
+    ; --- TEST PA2 ---
+    MOV AL, 04H     ; Binary 0000 0100
+    OUT DX, AL
+    CALL LONG_DELAY
 
-; --- DELAY FOR MULTIPLEXING ---
-DELAY_MUX:
-    PUSH CX
-    ; Increased delay here helps fix "Only one display working"
-    MOV CX, 0200H    
-WAIT:
-    LOOP WAIT
-    POP CX
+    ; --- TEST PA3 ---
+    MOV AL, 08H     ; Binary 0000 1000
+    OUT DX, AL
+    CALL LONG_DELAY
+
+    ; --- TEST PA4 ---
+    MOV AL, 10H     ; Binary 0001 0000
+    OUT DX, AL
+    CALL LONG_DELAY
+
+    ; --- TEST PA5 ---
+    MOV AL, 20H     ; Binary 0010 0000
+    OUT DX, AL
+    CALL LONG_DELAY
+
+    ; --- TEST PA6 ---
+    MOV AL, 40H     ; Binary 0100 0000
+    OUT DX, AL
+    CALL LONG_DELAY
+
+    JMP TEST_LOOP
+
+LONG_DELAY:
+    MOV CX, 0FFFFH  ; Very slow delay
+D1: LOOP D1
+    MOV CX, 0FFFFH  ; Do it twice so you have time to see
+D2: LOOP D2
     RET
