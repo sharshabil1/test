a0e243a5d073ecb689ef586455ef95502081cc64
sharshabil <122256306+sharshabil1@users.noreply.github.com>
2025-12-24 11:49:18 +0300

    Fix wiring and segment data for display
    Updated wiring and segment encoding for Student ID Scroll project.

--- ssd2.asm
+++ ssd2.asm
@@ -1,34 +1,40 @@
 ; =================================================================
-; Project: Student ID Scroll (222438835)
-; Wiring: CUSTOM SCRAMBLED (A=PA5, B=PA4, C=PA7, D=PA0, E=PA3, F=PA2, G=PA6)
-;         CAT = PB0
+; Project: Student ID Scroll (FIXED STANDARD WIRING)
+;
+; CORRECT WIRING:
+;   Port A (PA0-PA6): Segments A, B, C, D, E, F, G
+;   Port B (PB0):     CAT (Digit Select) -> 0=Right, 1=Left
+;
+; FIXES:
+;   - Removed 'CAT' from Port A (it was interfering with segments).
+;   - Moved 'CAT' control entirely to Port B (PB0).
+;   - Used Standard Hex Codes for the segments.
 ; =================================================================
 
 ORG 2000H
     JMP START
 
-; --- HEX CODE CALCULATIONS ---
-; '2' (A,B,D,E,G) -> Bits 5,4,0,3,6 -> 0111 1001 = 79H
-; '4' (B,C,F,G)   -> Bits 4,7,2,6   -> 1101 0100 = 0D4H
-; '3' (A,B,C,D,G) -> Bits 5,4,7,0,6 -> 1111 0001 = 0F1H
-; '8' (All)       -> Bits 7,6,5,4,3,2,0 -> 1111 1101 = 0FDH
-; '5' (A,C,D,F,G) -> Bits 5,7,0,2,6 -> 1110 0101 = 0E5H
-; Space           -> 00H
+; --- DATA TABLE (Standard Encoding) ---
+; A=Bit0, B=Bit1, C=Bit2, D=Bit3, E=Bit4, F=Bit5, G=Bit6
+;
+; '2' -> 5BH
+; '4' -> 66H
+; '3' -> 4FH
+; '8' -> 7FH
+; '5' -> 6DH
+; Space -> 00H
 
 CODES:
-    DB 79H      ; [0] '2'
-    DB 79H      ; [1] '2'
-    DB 79H      ; [2] '2'
-    DB 0D4H     ; [3] '4'
-    DB 0F1H     ; [4] '3'
-    DB 0FDH     ; [5] '8'
-    DB 0FDH     ; [6] '8'
-    DB 0F1H     ; [7] '3'
-    DB 0E5H     ; [8] '5'
-    
-    ; Leading/Trailing Spaces for smooth scroll
+    DB 5BH      ; [0] '2'
+    DB 5BH      ; [1] '2'
+    DB 5BH      ; [2] '2'
+    DB 66H      ; [3] '4'
+    DB 4FH      ; [4] '3'
+    DB 7FH      ; [5] '8'
+    DB 7FH      ; [6] '8'
+    DB 4FH      ; [7] '3'
+    DB 6DH      ; [8] '5'
     DB 00H      ; [9] Space
-    DB 00H      ; [10] Space
 
 START:
     ; 1. CONFIGURE 8255 
@@ -38,14 +44,14 @@ START:
 
 MAIN_LOOP:
     MOV SI, CODES    ; Point to start of ID
-    MOV CX, 10       ; 10 Steps (ID + Spaces)
+    MOV CX, 9        ; 9 digits to scroll
 
 SCROLL_SEQUENCE:
     PUSH CX          
 
-    ; Load pair of digits
-    MOV AL, [SI]     ; Left Digit
-    MOV BL, [SI+1]   ; Right Digit
+    ; Load the pair of digits
+    MOV AL, [SI]     ; Left Digit Data
+    MOV BL, [SI+1]   ; Right Digit Data
 
     ; --- SPEED ---
     MOV CX, 01FFH   
@@ -55,7 +61,7 @@ MULTIPLEX_LOOP:
     LOOP MULTIPLEX_LOOP
 
     POP CX           
-    INC SI           ; Next Digit
+    INC SI           ; Next digit
     LOOP SCROLL_SEQUENCE
     
     JMP MAIN_LOOP    
@@ -69,13 +75,14 @@ DISPLAY_PAIR:
     ; 1. DISPLAY RIGHT DIGIT (CAT PB0 = 0)
     ; ======================================
 
-    MOV DX, 0FFE2H   ; Port B
-    MOV AH, 00H      ; PB0 = 0
-    MOV AL, AH
+    ; A. Select Right Digit (Port B)
+    MOV DX, 0FFE2H   ; Port B Address
+    MOV AL, 00H      ; PB0 = 0 (Right)
     OUT DX, AL
 
-    MOV DX, 0FFE0H   ; Port A
-    MOV AL, BL       ; Load Right Digit
+    ; B. Send Segment Data (Port A)
+    MOV DX, 0FFE0H   ; Port A Address
+    MOV AL, BL       ; Load Right Data
     OUT DX, AL       
     
     CALL DELAY_MUX
@@ -84,14 +91,15 @@ DISPLAY_PAIR:
     ; 2. DISPLAY LEFT DIGIT (CAT PB0 = 1)
     ; ======================================
 
-    MOV DX, 0FFE2H   ; Port B
-    MOV AH, 01H      ; PB0 = 1
-    MOV AL, AH
+    ; A. Select Left Digit (Port B)
+    MOV DX, 0FFE2H   ; Port B Address
+    MOV AL, 01H      ; PB0 = 1 (Left)
     OUT DX, AL
 
-    MOV DX, 0FFE0H   ; Port A
-    POP AX           ; Restore Left Digit
-    PUSH AX          
+    ; B. Send Segment Data (Port A)
+    MOV DX, 0FFE0H   ; Port A Address
+    POP AX           ; Restore Left Data (from Stack)
+    PUSH AX          ; Push back
     OUT DX, AL       
 
     CALL DELAY_MUX
